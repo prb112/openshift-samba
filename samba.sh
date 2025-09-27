c3ade75cba8aa2361b39fe7f53fe45aa01d25650
Paul Bastide <pbastide@us.ibm.com>
2025-09-26 21:42:01 -0400

    fix: created the static userid
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- samba.sh
+++ samba.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-exec ionice -c 3 smbd -F --no-process-group </dev/null
+echo "Creating Samba users"
+adduser -s /sbin/nologin -h /home/samba -H -D USERNAME
+yes PASSWORD | smbpasswd -a USERNAME
+
+echo "Starting Samba 4"
+ionice -c 3 smbd --foreground --log-stdout --no-process-group < /dev/null
