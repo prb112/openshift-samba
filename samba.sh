0008b11587f6d761b6aabe6f1ae3ec541a39f837
Paul Bastide <pbastide@us.ibm.com>
2025-09-26 21:45:12 -0400

    fix: invalid flag for smbd
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- samba.sh
+++ samba.sh
@@ -5,4 +5,4 @@ adduser -s /sbin/nologin -h /home/samba -H -D USERNAME
 yes PASSWORD | smbpasswd -a USERNAME
 
 echo "Starting Samba 4"
-ionice -c 3 smbd --foreground --log-stdout --no-process-group < /dev/null
+ionice -c 3 smbd --foreground --debug-stdout --no-process-group < /dev/null
